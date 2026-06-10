972ec236d65f287566a2f337696f0dce99231ce9
JordyTshibssB <169159357+JordyTshibssB@users.noreply.github.com>
2026-06-10 20:45:18 +0400

    fix: update chart component to improve performance

--- components/ui/chart.tsx
+++ components/ui/chart.tsx
@@ -127,6 +127,8 @@ function ChartTooltipContent({
     indicator?: "line" | "dot" | "dashed"
     nameKey?: string
     labelKey?: string
+    payload?: RechartsPrimitive.TooltipPayload
+    label?: React.ReactNode
   }) {
   const { config } = useChart()
 
@@ -188,7 +190,7 @@ function ChartTooltipContent({
 
           return (
             <div
-              key={item.dataKey}
+              key={`${item.dataKey ?? item.name ?? index}`}
               className={cn(
                 "[&>svg]:text-muted-foreground flex w-full flex-wrap items-stretch gap-2 [&>svg]:h-2.5 [&>svg]:w-2.5",
                 indicator === "dot" && "items-center"
@@ -259,7 +261,10 @@ function ChartLegendContent({
   verticalAlign = "bottom",
   nameKey,
 }: React.ComponentProps<"div"> &
-  Pick<RechartsPrimitive.LegendProps, "payload" | "verticalAlign"> & {
+  Pick<
+    RechartsPrimitive.DefaultLegendContentProps,
+    "payload" | "verticalAlign"
+  > & {
     hideIcon?: boolean
     nameKey?: string
   }) {
